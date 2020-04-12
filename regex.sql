5054f81a6e21580d984fb2db35422a66ee649cbc
satishkumar432 <satishkumar432@gmail.com>
2020-04-12 00:00:56 -0700

    Create regex.sql

---
+++ regex.sql
@@ -0,0 +1,8 @@
+use sql_store;
+select * from customers 
+-- where first_name regexp 'ELKA|AMBUR'
+-- where last_name regexp 'EY$'
+-- or last_name regexp 'ON$'
+-- where last_name regexp'^my|se'
+-- or last_name regexp 'se' 
+-- where last_name regexp 'b[ru]'
